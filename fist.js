17cbf62ad1b74e20103e25e783ada542c7d4eb42
noamlew <65733811+noamlew@users.noreply.github.com>
2020-11-02 11:24:57 +0200

    Update fist.js

--- fist.js
+++ fist.js
@@ -3,13 +3,15 @@ const img = document.getElementById("image");
 const Punch = new Audio();
 Punch.src = "audio/Punch Sound Effects All Sounds (mp3cut.net).mp3";
 
+let setTime = setTimeout(() => console.log("welcome the game"), 10);
+
 document.addEventListener("mousedown", () => {
+    clearTimeout(setTime);
     img.src = "image/download (5).jpg";
     document.body.style.background = "red";
     Punch.play();
-})
-
-document.addEventListener("mouseup", () => {
-    img.src = "image/2602451-46.jpg";
-    document.body.style.background = "cornflowerblue";
+    setTime = setTimeout(() => {
+        img.src = "image/2602451-46.jpg";
+        document.body.style.background = "cornflowerblue";
+    }, 900);
 })
